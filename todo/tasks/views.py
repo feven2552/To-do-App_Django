f23c44446a246c172c02aa3e38e21d5408d1b0d5
feven <feven.25yi@gmail.com>
2024-01-02 23:13:41 +0300

    Update views.py

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
-# Create your views here.
 def index(request):
     tasks = Task.objects.all()
 
